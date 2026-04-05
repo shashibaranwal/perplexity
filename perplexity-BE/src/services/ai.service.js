8044eeee94834f4533944ed08f95776c31f0d09e
shashibaranwal <shashibaranwal09@gmail.com>
2026-04-06 00:16:49 +0545

    add internet search feature

--- perplexity-BE/src/services/ai.service.js
+++ perplexity-BE/src/services/ai.service.js
@@ -1,6 +1,8 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import { ChatMistralAI } from "@langchain/mistralai";
-import { HumanMessage, SystemMessage, AIMessage } from "langchain";
+import { HumanMessage, SystemMessage, AIMessage, tool, createAgent } from "langchain";
+import * as z from "zod";
+import { searchInternet } from "./internet.service.js";
 
 const geminiModel = new ChatGoogleGenerativeAI({
   model: "gemini-2.5-flash-lite",
@@ -12,17 +14,35 @@ const mistralModel = new ChatMistralAI({
   apiKey: process.env.MISTRAL_API_KEY
 });
 
+const searchInternetTool = tool(
+  searchInternet,
+  {
+    name: "search_internet",
+    description: "Search the internet for latest information",
+    schema: z.object({
+      query: z.string().describe("The query to search the internet for")
+    })
+  }
+)
+
+const agent = createAgent({
+  model: geminiModel,
+  tools: [searchInternetTool],
+})
+
 export const generateResponse = async (messages) => {
 
-  const response = await geminiModel.invoke(messages.map( msg => {
-    if(msg.role == 'user'){
-      return new HumanMessage(msg.content);
-    }
-    else{
-      return new AIMessage(msg.content);
-    }
-  }));
-  return response.text;
+  const response = await agent.invoke({
+    messages: messages.map( msg => {
+      if(msg.role == 'user'){
+        return new HumanMessage(msg.content);
+      }
+      else{
+        return new AIMessage(msg.content);
+      }
+    })
+  });
+  return response.messages[response.messages.length - 1].text;
 }
 
 export const generateChatTitle = async (message) => {
---
+++ perplexity-BE/src/services/internet.service.js
@@ -0,0 +1,15 @@
+import { tavily as Tavily } from "@tavily/core";
+
+const tavily = Tavily({ 
+  apiKey: process.env.TAVILY_API_KEY,
+
+ });
+
+
+export const searchInternet = async ({query}) => {
+  return await tavily.search(query, {
+    maxResults: 4,
+    searchDepth: "advanced"
+  });
+
+}
